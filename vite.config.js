be54971958542f90f6bf1e3d75e43900131044a8
IzacLuthfi <izacluthfi12@gmail.com>
2026-05-07 16:21:58 +0700

    commit 9
    commit 9

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['Logo.png', 'favicon.ico', 'robots.txt'],
+      manifest: {
+        name: 'MyLove Izac & Lian',
+        short_name: 'MyLove I&L',
+        description: 'Aplikasi Kenangan Manis Izac dan Lian',
+        theme_color: '#FAFAFA',
+        background_color: '#FAFAFA',
+        display: 'standalone', // Membuatnya tampil seperti aplikasi asli tanpa bar browser
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'Logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'Logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'Logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
